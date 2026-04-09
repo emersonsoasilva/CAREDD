79b07c47dba171aab4bdd89aa47828a820b9752d
emersonsoasilva <emersonsoares141@gmail.com>
2026-04-08 21:46:24 -0300

    update: tarefa05 (complete)

--- RECURSIVIDADE/TAREFA/tarefa05.java
+++ RECURSIVIDADE/TAREFA/tarefa05.java
@@ -1,12 +1,18 @@
 void main() {
-
+     int[] vet = {1, 2, 3, 40, 52, 0, 2};
+    System.out.println(menorElemento(vet, 6));
 }
 
-int menorElemento(int t, int[] v) {
-    if(t == 0) {
-        return 0;
+int menorElemento(int v[] , int t) {
+    if(t > 0) {
+        int temp = menorElemento(v, t-1);
+
+        if(temp < v[t]) {
+            return temp;
+        }
+
+        return v[t];
     }
-    else {
-        return v[t-1] + menorElemento(t-1, v);
-    }
+
+    return v[t];
 }
