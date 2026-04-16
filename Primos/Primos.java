5a48a353e3980e513daafedeb4575107a46b9f20
emersonsoasilva <emersonsoares141@gmail.com>
2026-04-15 21:13:46 -0300

    add: primos complete

--- Primos/Primos.java
+++ Primos/Primos.java
@@ -13,28 +13,74 @@ public class Primos {
         for (int i = 0; i <= N; i++) {
             inserir(i);
         }
-        primos(N);
-        
+
+        System.out.println("Antes");
         exibir(inicio);
+
+                System.out.println("Existem "+tamanho(inicio)+" numeros primos entre 0 e "+N);
+        
+        System.out.println(" Depois");
+        primos(N);
+        exibir(inicio);
+
         System.out.println("Existem "+tamanho(inicio)+" numeros primos entre 0 e "+N);
     }
     
     private static void primos(int n){
-        if (n > 1) {
-            for (int i = 2; i * i <= n; i++) {
-                if
+
+        No temp = inicio;
+        No anterior = null;
+
+        while(temp != null) {
+
+            int valor = temp.valor;
+            int divisores = 0;
+
+            for (int i = 1; i <= valor; i++) {
+                if (valor % i == 0) {
+                    divisores++;
+                }
             }
+
+            if (divisores == 2) {
+                if (anterior == null) {
+                    inicio = temp.prox;
+                    temp = inicio;
+                } else {
+                    anterior.prox = temp.prox;
+                    temp = temp.prox;
+                }
+            } else {
+                anterior = temp;
+                temp = temp.prox;
+            }
+
         }
 
-        else {
 
-        }
       
     }
     
     private static int tamanho(No temp){
-       
-        return 0;
+       if (temp == null) {
+            return 0;
+       }
+
+       int valor = temp.valor;
+       int divisores = 0;
+
+       for (int i = 1; i <= valor; i++) {
+            if (valor % i == 0) {
+                divisores++;
+            }
+       }
+
+       if (divisores == 2) {
+            return 1 + tamanho(temp.prox);
+       } else {
+            return tamanho(temp.prox);
+       }
+
     }    
     
     
@@ -65,6 +111,13 @@ public class Primos {
     }
         
     private static void exibir(No temp) {
-       
+
+        No tmp = temp;
+
+        while(tmp != null) {
+            System.out.println(tmp.valor);
+            tmp = tmp.prox;
+        }
+        System.out.println("Vazio...");
     }
 }
